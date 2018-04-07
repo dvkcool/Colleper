8ea0a85594880a39f14a5eea61a01787d8ed44ba
unknown <chetanasahu1997@gmail.com>
2018-04-07 22:45:49 +0530

    popup added

--- microservices/api/src/public/jquery/ff.js
+++ microservices/api/src/public/jquery/ff.js
@@ -3,7 +3,6 @@ function fettch(){
 
   var a = document.getElementById('em').value;
   var b = document.getElementById('pass').value;
-  console.log("a: "+ a+ " B: "+ b);
   fetch("https://auth.controversial68.hasura-app.io/v1/login",{
     method: 'POST',
     headers: {
@@ -11,31 +10,32 @@ function fettch(){
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-  
+
   "provider": "username",
   "data":{
   "username":""+a,
   "password":""+b
-  
+
   }
   })
   }
-  
+
   )
   .then((res) => res.json())
   .then((resp) => {
-    console.log(resp);
+
     if(resp.code=="invalid-creds"){
       alert("Invalid login");
     }
     else{
-      window.location = "t2.html";
-    }
+	var name = resp.username;
+  localStorage.setItem('user', name);
+  window.location="index.html";
+}
+ })
+
+
+
+
+
   }
-  )
-  
-  
-  
-  
-  }
-  
