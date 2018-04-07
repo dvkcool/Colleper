fba8331f19e614821dcfb9000ae62c4ace79e5c3
unknown <chetanasahu1997@gmail.com>
2018-04-07 13:47:46 +0530

    menu button

---
+++ microservices/api/src/public/jquery/menu3.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#main").click(function(){
+    $("#main").animate({height:'800px',width:'800px'});
+    $("#icon").fadeOut();
+    $("#menu ul").delay(300).animate({opacity:'1'});
+    $("#menu ul").css("visibility","visible");
+    $("#bg").delay(200).animate({opacity:"1"});
+    $("#bg").css("visibility","visible");
+  });
+  $("#bg").click(function(){
+    $("#main").animate({height:'50px',width:'50px'});
+    $("#icon").fadeIn();
+    $("#menu ul").animate({opacity:'0'});
+    $("#menu ul").css("visibility","hidden");
+    $("#bg").animate({opacity:"0"});
+    $("#bg").css("visibility","hidden");
+  });
+});
