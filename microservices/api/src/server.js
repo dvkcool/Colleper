19a5b6d794aec74fb222af7dba6e28b48a7c3b06
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-04-07 13:04:50 +0530

    trying first html page from index folde

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
 require('request-debug')(request);
 
-var hasuraExamplesRouter = require('./hasuraExamples');
+/*var hasuraExamplesRouter = require('./hasuraExamples');
 
 var server = require('http').Server(app);
 
@@ -34,3 +34,7 @@ app.use('/', hasuraExamplesRouter);
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+*/
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
