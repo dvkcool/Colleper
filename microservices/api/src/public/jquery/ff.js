faa8bafe682138fdd12772cc14030df0ba7618da
unknown <chetanasahu1997@gmail.com>
2018-04-08 12:23:50 +0530

    disabling the button

--- microservices/api/src/public/jquery/ff.js
+++ microservices/api/src/public/jquery/ff.js
@@ -1,9 +1,55 @@
 
+
+function fetttch(){
+
+ var c = document.getElementById('em1').value;
+ var d = document.getElementById('pass1').value;
+
+
+ fetch("https://auth.alias14.hasura-app.io/v1/login",{
+   method: 'POST',
+   headers: {
+     Accept: 'application/json',
+     'Content-Type': 'application/json',
+   },
+   body: JSON.stringify({
+
+ "provider": "username",
+ "data":{
+ "username":""+c,
+ "password":""+d
+
+ }
+ })
+ }
+ )
+ .then((res) => res.json())
+ .then((resp) => {
+
+   if(resp.code=="invalid-creds"){
+     alert("Invalid login");
+   }
+   else{
+ var name = resp.username;
+  //     alert("roles: "+ resp.hasura_roles[0]);
+       localStorage.setItem("role",resp.hasura_roles[0] );
+ window.location="index.html";
+ }
+ })
+ }
+
+
+
+
+
+
+
+
 function fettch(){
 
   var a = document.getElementById('em').value;
   var b = document.getElementById('pass').value;
-  fetch("https://auth.controversial68.hasura-app.io/v1/login",{
+  fetch("https://auth.alias14.hasura-app.io/v1/login",{
     method: 'POST',
     headers: {
       Accept: 'application/json',
@@ -19,8 +65,7 @@ function fettch(){
   }
   })
   }
-
-  )
+)
   .then((res) => res.json())
   .then((resp) => {
 
@@ -29,13 +74,9 @@ function fettch(){
     }
     else{
 	var name = resp.username;
-  localStorage.setItem('user', name);
+  localStorage.setItem("role",resp.hasura_roles[0] );
+      //  alert("roles: "+ resp.hasura_roles[0]);
   window.location="index.html";
 }
  })
-
-
-
-
-
-  }
+}
